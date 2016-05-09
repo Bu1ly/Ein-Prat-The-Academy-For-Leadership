7a4c4486506eafb273b4aedba26532b632f9d1d1
Danny Volpov <volpovdanny89@gmail.com>
2016-05-09 17:06:40 +0300

    connect mongo to app.js + new seniourList ejs file in views

--- Ein_Prat/app.js
+++ Ein_Prat/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
-var mongo = require('mongodb');
+var mongodb = require('mongodb');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
