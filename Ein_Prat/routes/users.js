026ca3b1832a742145dfbf36f2c1e53e26a690b1
Danny Volpov <volpovdanny89@gmail.com>
2016-05-09 12:33:06 +0300

    files for project structure

--- Ein_Prat/routes/users.js
+++ Ein_Prat/routes/users.js
@@ -3,27 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Users Section here..');
+  res.send('respond with a resource');
 });
-
-//OBJECT FACTORY
-var users =  module.exports = function() {
-  return {
-    id: "",
-    firstName: "",
-    lastName: ""
-  }
-};
-
-//EXAMPLE
-var user1 = users();
-var user2 = users();
-
-user1.name = "Danny";
-user1.id = "306319138"
-user2.id = "00000000";
-
-
-console.log("User1 name is: " + user1.name + " and id is: " + user1.id + " user2 id is: " + user2.id + " but he got no name so user 2 name is: " + user2.name);
 
 module.exports = router;
