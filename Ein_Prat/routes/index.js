2934e3651281eeffef3036171a33f97775b0994a
Danny Volpov <volpovdanny89@gmail.com>
2016-05-15 09:54:38 +0300

    connect mongo to app.js + new seniourList ejs file in views

--- Ein_Prat/routes/index.js
+++ Ein_Prat/routes/index.js
@@ -7,31 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ein_Prat' });
 });
 
-router.get('theList',function(req,res){
-  var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://localhost:27017/Ein_Prat';
-  MongoClient.connect(url,function(error,db){
-    if(error)
-      console.log('Unale to connect to the mongo',error);
-    else{
-      console.log("Connection established");
 
-      var collection = db.collection('seniors');
-
-      collection.find({}).toArray(function (error,result){
-        if(error)
-          res.send(error);
-        else if(result.length){
-          res.render('seniorList',{
-            "seniourList" : result
-          });
-        }else{
-          res.send('No docs found');
-        }
-        db.close();
-      });
-    }
-  });
-});
 
 module.exports = router;
