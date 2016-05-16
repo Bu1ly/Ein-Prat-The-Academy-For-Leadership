117ed63ffa29e6f5a46db4bbf579317fb93f1215
Danny Volpov <volpovdanny89@gmail.com>
2016-05-16 12:21:34 +0300

    app listen func update + server.js file

--- Ein_Prat/server.js
+++ Ein_Prat/server.js
@@ -1,6 +1,7 @@
 var app = require('express'); // get express for server init
 var path = require('path');// get the path
-
+var mongoose = require('mongoose');// DB connections
+var Schema = mongoose.Schema;//to create schemas
 
 //init for using POST calls
 var bodyParser = require('body-parser');
@@ -9,11 +10,29 @@ app.use(bodyParser.json()); //read json data
 
 
 //static routes init
-app.use('/js', express.static('public/js'));
+
+app.use('/javascripts', express.static('public/javascripts'));
 app.use('/css', express.static('public/css'));
 app.use('/images', express.static('public/images'));
 app.use('/font', express.static('public/font'));
+app.use('/stylesheets', express.static('public/stylesheets'));
 app.use(express.static('views'));
+
+
+//Connect to DB
+
+var connection = mongoose.createConnection('mongodb://Bu1ly:danivolp89@ds049624.mlab.com:49624/ein_prat',function () {
+
+    console.log("App is now connected to Mlab DB");
+});
+
+//Create schema
+
+var seniors = new Schema({
+    name: String,
+    identity: String,
+    lastName: String
+});
 
 
 
