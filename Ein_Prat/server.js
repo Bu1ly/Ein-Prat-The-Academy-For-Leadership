964a03c4012040f191acc500a0e08acaad97744d
Graos <yerson28890@gmail.com>
2016-05-16 13:53:10 +0300

    create new seniors

--- Ein_Prat/server.js
+++ Ein_Prat/server.js
@@ -5,10 +5,11 @@ var Schema = mongoose.Schema;//to create schemas
 
 //init for using POST calls
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));//read URL encoded
+app.use(bodyParser.urlencoded({ extended: false }));//read URL encoded
 app.use(bodyParser.json()); //read json data
 
-
+// exports
+var exports = module.exports = {};
 
 //static routes init
 
@@ -35,11 +36,51 @@ var seniors = new Schema({
     lastName: String
 });
 
-//new Collection
+// Connect collection to schema
+var Senior = connection.model('Senior',seniors);
 
-var Seniour = connection.model('Senior', seniors);
+// Main route
+app.get('/', function (req,res) {
+    res.sendfile(path.join(__dirname + "/views/index.ejs"));
+});
+
+// --------API CALLS --------
+//Register new Users
+app.post('reg', function(req,res){
+   var jason = req.body; // get the user data
+    console.log(jason); //print for debug
+
+   // create senior object
+    var seniorJason = {
+        name : jason.name,
+        lastName : jason.lastName,
+        identity : jason.identity
+    }
+
+    // create new DB instance
+    var newSenior = new Senior(seniorJason);
+
+    // save the newSenior to the DB
+    newSenior.save(function(err){
+        if(err)
+            res.status(500).end("Error");
+        else
+            res.status(200).end("Added", seniorJason, "to Seniors DB");
+    })
+});
+
+// Delete some User
+app.post('/delete', function (req,res) {
+    var SeniorJason = req.body; // get the user data
+    console.log(SeniorJason); //print for debug
 
 
-module.exports('server.js');
+
+})
+
+
+
+
+//module.exports(server);
 
 
