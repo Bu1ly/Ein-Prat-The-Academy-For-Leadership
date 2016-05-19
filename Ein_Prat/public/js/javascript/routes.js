aa3c593305a308135f8e0e95b00ec4c36ebfcdf2
Net4nel <Net4nel@gmail.com>
2016-05-19 19:36:03 +0300

    few tiny changes

--- Ein_Prat/public/js/javascript/routes.js
+++ Ein_Prat/public/js/javascript/routes.js
@@ -5,9 +5,6 @@ angular.module('mainApp')
     $routeProvider.when('/home_page', {
         templateUrl: '/views/home_page.html'
     })
-        .when('/home_page', {
-            templateUrl: '/views/home_page.html'
-        })
         .when('/senior_search', {
             templateUrl: '/views/senior_search.html'
         })
@@ -33,9 +30,9 @@ angular.module('mainApp')
         .when('/app_settings', {
             templateUrl: '/views/app_settings.html'
         })
-        .when('/', {
-            templateUrl: '/views/home_page.html'
-        })
+        // .when('/', {
+        //     templateUrl: '/views/home_page.html'
+        // })
         .otherwise({redirectTo: '/views/not_yet_exists.html'});
 
 
