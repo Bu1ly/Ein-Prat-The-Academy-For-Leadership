52fe8c2dbc51147601e794f0689abf5c64464c11
Net4nel <Net4nel@gmail.com>
2016-05-19 19:28:13 +0300

    - routes.js added - pages added - ng-view sets

--- Ein_Prat/public/js/javascript/routes.js
+++ Ein_Prat/public/js/javascript/routes.js
@@ -1,7 +1,42 @@
 angular.module('mainApp')
 .config(function($routeProvider){
 
-    $routeProvider.when('/events', {
-        templateUrl: '/views/events.html'
+    // RIGHT NAV
+    $routeProvider.when('/home_page', {
+        templateUrl: '/views/home_page.html'
     })
-})
+        .when('/home_page', {
+            templateUrl: '/views/home_page.html'
+        })
+        .when('/senior_search', {
+            templateUrl: '/views/senior_search.html'
+        })
+        .when('/studies', {
+            templateUrl: '/views/studies.html'
+        })
+        .when('/sales', {
+            templateUrl: '/views/sales.html'
+        })
+        .when('/events', {
+            templateUrl: '/views/events.html'
+        })
+        .when('/blank_page', {
+            templateUrl: '/views/blank_page.html'
+        })
+        .when('/info', {
+            templateUrl: '/views/info.html'
+        })
+        // UPPER NAV
+        .when('/change_info', {
+            templateUrl: '/views/blank_page.html'
+        })
+        .when('/app_settings', {
+            templateUrl: '/views/app_settings.html'
+        })
+        .when('/', {
+            templateUrl: '/views/home_page.html'
+        })
+        .otherwise({redirectTo: '/views/not_yet_exists.html'});
+
+
+});
