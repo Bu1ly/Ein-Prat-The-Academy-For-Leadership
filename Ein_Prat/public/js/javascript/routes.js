44206de881359a84c1aa4696f08d9af5068ef9aa
Net4nel <Net4nel@gmail.com>
2016-05-20 17:27:21 +0300

    - ng-view fixed - route.js fixed
    **need to fix "Otherwise" option.

--- Ein_Prat/public/js/javascript/routes.js
+++ Ein_Prat/public/js/javascript/routes.js
@@ -3,39 +3,39 @@ angular.module('mainApp')
 
     // RIGHT NAV
     $routeProvider.when('/home_page', {
-        templateUrl: '/views/home_page.html'
+        templateUrl: 'views/home_page.html'
     })
         .when('/senior_search', {
-            templateUrl: '/views/senior_search.html'
+            templateUrl: 'views/senior_search.html'
         })
         .when('/studies', {
-            templateUrl: '/views/studies.html'
+            templateUrl: 'views/studies.html'
         })
         .when('/sales', {
-            templateUrl: '/views/sales.html'
+            templateUrl: 'views/sales.html'
         })
         .when('/job_offers', {
-            templateUrl: '/views/job_offers.html'
+            templateUrl: 'views/job_offers.html'
         })
         .when('/events', {
-            templateUrl: '/views/events.html'
+            templateUrl: 'views/events.html'
         })
         .when('/blank_page', {
-            templateUrl: '/views/blank_page.html'
+            templateUrl: 'views/blank_page.html'
         })
         .when('/info', {
-            templateUrl: '/views/info.html'
+            templateUrl: 'views/info.html'
         })
 
         // UPPER NAV
         .when('/change_info', {
-            templateUrl: '/views/blank_page.html'
+            templateUrl: 'views/blank_page.html'
         })
         .when('/app_settings', {
-            templateUrl: '/views/app_settings.html'
+            templateUrl: 'views/app_settings.html'
         })
         .when('/', {
-            templateUrl: 'views/bird.html'
+            templateUrl: 'views/home_page.html'
         })
         .otherwise({redirectTo: 'views/404.html'});
 
