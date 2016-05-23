0c087d8e709daa185c1f24f0b866f5975e92fdb6
Net4nel <Net4nel@gmail.com>
2016-05-23 14:44:49 +0300

    minor changes

--- Ein_Prat/public/js/controllers/registerCtrl/controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('mainApp')
-    .controller('RegisterCtrl' ,['$scope', function($scope){
-        /*
-         h = the height of the page
-         */
-        $scope.alert = "Hello Garus";
-    }]);
