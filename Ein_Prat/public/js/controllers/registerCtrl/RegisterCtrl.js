e3205135dd8be8a34e7ad61eb391b9c70d71b18f
Net4nel <Net4nel@gmail.com>
2016-05-24 13:00:59 +0300

    minor changes

--- Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
+++ Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
@@ -1,6 +1,15 @@
 angular.module('mainApp')
     .controller('RegisterCtrl', [function($http) {
-        this.newUser = function(user){
-            $http({method: 'POST', url: '/', data: user});
+        this.user = {};
+        this.users = [];
+
+        this.newUser = function(){
+            console.log(this.user);
+            $http({method: 'POST', url: '/reg', data: this.user})
+                .then(function(data){
+                    this.users.push(this.user);
+                    this.user = {};
+
+                });
         };
     }]);
