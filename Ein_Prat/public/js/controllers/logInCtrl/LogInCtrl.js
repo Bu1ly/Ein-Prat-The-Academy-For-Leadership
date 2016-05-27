aff53713c58c00900d18eda7f3d6471b6a5afa87
Net4nel <Net4nel@gmail.com>
2016-05-27 12:28:08 +0300

    bootstrap icon fixed. login controller functionallity.

--- Ein_Prat/public/js/controllers/logInCtrl/LogInCtrl.js
+++ Ein_Prat/public/js/controllers/logInCtrl/LogInCtrl.js
@@ -1,6 +1,17 @@
 angular.module('mainApp')
-    .controller('LogInCtrl', [function($http) {
-        this.checkId = function(user){
-            $http({method: 'GET', url: '/', data: user.id});
+    .controller('LogInCtrl', ['$http', function($http) {
+        // this.user = {};
+        // this.users = [];
+
+        this.checkUser = function(){
+            console.log(this.user);
+            $http({method: 'GET', url: '/log', data: this.user})
+                // .then(function(data){
+                    // this.users.push(this.user);
+                    // this.user = {};
+                .then(function(data){
+                    console.log("{{user.name}}" + "is exists!")
+
+                });
         };
     }]);
