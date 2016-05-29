6e7e63091dec75f0e0b63b2920b84953841d37b0
Net4nel <Net4nel@gmail.com>
2016-05-29 11:55:38 +0300

    ".then" Console logs "Success".  - for checking the db after dani's server update.

--- Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
+++ Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
@@ -7,6 +7,7 @@ angular.module('mainApp')
             console.log(this.user);
             $http({method: 'POST', url: '/reg', data: this.user})
                 .then(function(data){
+                    console.log("Success");
                     this.users.push(this.user);
                     this.user = {};
 
