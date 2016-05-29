e81514ba757cef33e724ef639f457afb3d8243dd
Net4nel <Net4nel@gmail.com>
2016-05-29 14:31:09 +0300

    register sends real user now.

--- Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
+++ Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
@@ -2,18 +2,20 @@ angular.module('mainApp')
     .controller('RegisterCtrl', ['$http', function($http) {
         this.user = {};
         this.users = [];
+        var user = this.user;
+        var users_array = this.users;
 
         this.newUser = function(){
             console.log(this.user);
             $http({method: 'POST', url: '/reg', data: this.user})
                 .then(function(data){
                     console.log("Success, the users inside db should be: ");
-                    this.users.push(this.user);
-                    console.log(users);
+                    users_array.push(user);
+                    console.log(users_array[0]);
+
                     this.user = {};
 
                 })
             .catch(function() { console.log("Unsuccessful") });
         };
-        // return user;
     }]);
