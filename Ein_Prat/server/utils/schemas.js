8837669f2233d474d7ec8b23b666072363588f45
Danny Volpov <volpovdanny89@gmail.com>
2016-05-31 18:52:08 +0300

    added parameters to Senior Schema and updated the /reg func to get those params.

--- Ein_Prat/server/utils/schemas.js
+++ Ein_Prat/server/utils/schemas.js
@@ -1,15 +1,30 @@
 //include variables and exists libraries
 var mongoose = require('mongoose');// DB connections
 var Schema = mongoose.Schema;//to create schemas
-var connection = require('./connectDB');
+//var connection = require('./connectDB');
 
 
+//Mongoose DB connection
+var connection = mongoose.createConnection('mongodb://danny:danivolp89@ds049624.mlab.com:49624/ein_prat',function (error) {
+    console.log("Trying to connect to the Mlab DB....\n");
 
-// -- Create schema --
+    if(error){
+        console.log("Warning! Error occurred!\n\n");
+        console.log(error.name, "<- Is the error name\n", error.message , "<- Is the error message");
+    }
+    else{
+        console.log("App is now connected to Mlab DB");
+    }
+});
+
+// -- Create Senior schema --
 var senior = new Schema({
-    name: String,
+    firstName: String,
     lastName: String,
-    identity: String
+    identity: String,
+    sis: String,
+    Email: String
+    //session : date.now()
 });
 
 // -- Connect collection to schema --
