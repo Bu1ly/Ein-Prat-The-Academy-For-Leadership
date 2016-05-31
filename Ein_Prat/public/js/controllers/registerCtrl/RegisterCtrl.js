c19687f7254d5e81369b2f352b6b17e37339c572
Net4nel <Net4nel@gmail.com>
2016-05-31 14:25:57 +0300

    register ng-click and send to DB works. NEXT STEP:  - unactivate button when details are wrong.  - check email validation.

--- Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
+++ Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
@@ -1,15 +1,24 @@
 angular.module('mainApp')
     .controller('RegisterCtrl', ['$http','$scope', function($http,$scope) {
         $scope.user = {
+            fname: "",
+            lname: "",
+            email: "",
             id: "",
             password: ""
         };
 
         $scope.newUser = function(){
+            console.log($scope.user);
             $http({method: 'POST', url: '/reg', data: $scope.user})
                 .then(function(data){
                     console.log("Inserted to DB");
                 })
             .catch(function() { console.log("Unsuccessful") });
         };
+        $scope.confirmPass = function(){
+
+        }
+
+
     }]);
