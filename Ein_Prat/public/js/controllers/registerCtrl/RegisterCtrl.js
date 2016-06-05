d0bee76ee5728761afd309ba41e9e0a5534d9c71
assaf <assafas@gmail.com>
2016-06-05 13:22:31 +0300

    confim password alert - "password does not match"

--- Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
+++ Ein_Prat/public/js/controllers/registerCtrl/RegisterCtrl.js
@@ -9,6 +9,14 @@ angular.module('mainApp')
             sis: ""
         };
 
+        $scope.data = {
+            confirmPassword : ''
+        };
+
+        var clearData = function() {
+            $scope.data = {};
+        };
+
         //===============================
         //
         //
@@ -34,6 +42,17 @@ angular.module('mainApp')
 
 
         $scope.newUser = function(){
+
+            if($scope.user.sis  !==  $scope.data.confirmPassword) {
+                //here you need to send him back to fill signUP request(same page)
+                console.log('password does not match');
+                alert('password does not match');
+                clearData();
+
+                return;
+            }
+
+
             console.log($scope.user);
             $http({method: 'POST', url: '/reg', data: $scope.user})
                 .then(function(data){
