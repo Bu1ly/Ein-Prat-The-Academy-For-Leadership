040b4c5ecfd4a5d8aaa9e1a8bf0ed15ece4b2b99
Ori Halyo <Orihalyo123@gmail.com>
2016-06-10 01:32:05 +0300

    Fixing problem with defining controller. tabale and adding new job controller completed with css.
    good night :)

--- Ein_Prat/public/js/controllers/JobCtrl/jobctrl.js
+++ Ein_Prat/public/js/controllers/JobCtrl/jobctrl.js
@@ -7,74 +7,41 @@ function drop() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
+function addnew() {
+    document.getElementById('add').style.display = "block";
+
 }
 
-//--------------------Filter Controller---------------------------
 
 
-//var app = angular.module('app', []);
-//app.controller('jobCtrl', ['$scope', function($scope) {
-//    
-//    $scope.jobs=[];
-//    
-////    $scope.addJob = function() {
-////        $scope.jobs.push({'position'.$scope.position, 'category'.$scope.category,'location'.$scope.location, 'CVs'.$scope.CVs});
-////    }
-//    
-//    $scope.addJob = function() {
-//      
-//        $scope.jobs.push({ position: $scope.position, category: $scope.category, location: $scope.location, CVs: $scope.CVs });
-//    };
-//    
-//    $scope.position='';
-//    $scope.category='';
-//    $scope.location='';
-//    $scope.CVs='';
-//        
-//    }]);
+//--------------------Dynamic Table Controller---------------------------
 
-
-var app = angular.module('job', []);
-app.controller('jobCtrl', ['$scope', function($scope) {
+var app = angular.module('mainApp', []);
+    app.controller('jobCtrl', ['$scope', function($scope) {
 
     $scope.jobs = [
-        { 'position':'slut', 'category': 'Viral',
+        { 'position':'eng', 'category': 'hitech',
             'location':'jerusalem',
             'CVs': '123-2343-44'
         }
     ];
 
-    $scope.addJob = function() {
 
-//        if($scope.newcontact.id == null) {
-//        //if this is new contact, add it in contacts array
-//        $scope.newcontact.id = uid++;
-//        $scope.contacts.push($scope.newcontact);
-//        } else {
-//        //for existing contact, find this contact using id
-//        //and update it.
-//        for(i in $scope.contacts) {
-//            if($scope.contacts[i].id == $scope.newcontact.id) {
-//            $scope.contacts[i] = $scope.newcontact;
-//            }
-//        }                
-//        }
-//         
-//        //clear the add contact form
-//        $scope.newcontact = {};
-    };
+        $scope.addJob = function() {
+            $scope.jobs.push({'position':$scope.position, 'category':$scope.category,'location':$scope.location, 'CVs':$scope.CVs});
+        }
+
+        $scope.addJob = function() {
+
+            $scope.jobs.push({ position: $scope.position, category: $scope.category, location: $scope.location, CVs: $scope.CVs });
+
+            $scope.position='';
+            $scope.category='';
+            $scope.location='';
+            $scope.CVs='';
+        };
+
 
 }]);
 
