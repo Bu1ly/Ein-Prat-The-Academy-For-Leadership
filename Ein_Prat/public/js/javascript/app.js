20d8f87aa695e2034ceb34789093c529a86a7fd6
assaf <assafas@gmail.com>
2016-06-10 08:20:35 +0300

    login modal popup
    still not connect to the server

--- Ein_Prat/public/js/javascript/app.js
+++ Ein_Prat/public/js/javascript/app.js
@@ -18,3 +18,23 @@ var mainApp = angular.module('mainApp', ['ngRoute', 'ngMessages'])
         $scope.name="גרשון גראוס";
 });
 
+$('#myModal').on('hide.bs.modal', function (e) {
+    var button = e.relatedTarget;
+    if (button != null)
+    {
+        alert("Launch Button ID='" + button.id + "'");
+    }
+})
+
+
+$('#myModal2').click(function() {
+    alert("גרשון גראוס היקר, נא להרשם!");
+
+    // $('#myModal').modal('hide');
+})
+
+$('#myModalSave').click(function() {
+    alert("התחברת בהצלחה   - כמובן שעדיין אין בדיקה אמיתית מול השרת, צריך לסדר עוד");
+     $('#myModal').modal('hide');
+});
+
