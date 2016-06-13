dd282117f48aa520a791e402676677589c6a7559
assaf <assafas@gmail.com>
2016-06-13 14:04:27 +0300

    login work from the modal

--- Ein_Prat/public/js/javascript/app.js
+++ Ein_Prat/public/js/javascript/app.js
@@ -32,87 +32,44 @@ $('#myModal2').click(function() {
 
     // $('#myModal').modal('hide');
 })
-
-$('#myModalSave').click(function() {
-    alert("התחברת בהצלחה   - כמובן שעדיין אין בדיקה אמיתית מול השרת, צריך לסדר עוד");
-     $('#myModal').modal('hide');
-});
+//
+// $('#myModalSave').click(function() {
+//     alert("התחברת בהצלחה   - כמובן שעדיין אין בדיקה אמיתית מול השרת, צריך לסדר עוד");
+//      $('#myModal').modal('hide');
+// });
 
 // var mainApp = angular.module('mainApp', []);
+angular.module('mainApp').controller('igCtrl',['$http','$scope', function ($http,$scope) {
 
-//////////////////////////////////////////////////////////////////////// TEMP
-
-angular.module('mainApp').controller('igCtrl', function ($scope) {
-
-
-
-    $scope.email = "";
-    $scope.pwd = "";
-    $scope.loggedIn = false;
-    $scope.loggingIn = false;
-
-    $scope.showLogin = function () {
-        $scope.loggingIn = true;
+    console.log("test6");
+    $scope.user = {
+        identity: "",
+        sis: ""
     };
 
-    $scope.logout = function () {
-        // do your logout logic
-        $scope.user = null;
-        $scope.loggedIn = false;
-    };
-
-    $scope.login = function () {
-        // do your login logic
-        $scope.loggingIn = false;
-        $scope.loggedIn = true;
-    };
-});
-
-mainApp.directive('igLogin', function () {
-    return {
-        restrict: 'E',
-        replace: true,
-        template: '<div>' +
-        '  <div class="modal fade" id="loginModal" tabindex="-1" + role = "dialog" aria-labelledby = "myModalLabel" aria-hidden = "true" > ' +
-        '    <div class = "modal-dialog" > ' +
-        '      <form name = "form" ng - submit = "submit()" > ' +
-        '        <div class = "modal-content" > ' +
-        '          <div class = "modal-header" > ' +
-        '            <button type="button" class = "close" data-dismiss="modal" aria-hidden="true" ng-click="cancel()" > Cancel </button>' +
-        '              <h3> </h3 > ' +
-        '          </div>' +
-        '          <div class="modal-body">' +
-        '            <table border="0"><tr><td>Email: </td><td><input type="email" ng-model="email"></input > </td></tr> ' +
-        '            <tr><td>Password: </td><td><input type = "password" ng-model = "pwd" > </input></td></tr>' +
-        '            <tr><td colspan="2"><input type="submit" class="btn btn-primary" id="submit" ng-click="submit()" value="Login"></input ></td></tr></table> ' +
-        '          </div>' +
-        '        </div > ' +
-        '      </form>' +
-        '    </div > ' +
-        '  </div>' +
-        '</div > ',
-
-
-        controller: function ($scope) {
-
-            $scope.submit = function() {
-                $scope.login();
-                $("#loginModal").modal('hide');
-            };
-
-            $scope.cancel = function() {
-                $scope.loggingIn = false;
-                $("#loginModal").modal('hide');
-            };
-
-            $scope.$watch('loggingIn', function() {
-                if ($scope.loggingIn) {
-                    $("#loginModal").modal('show');
+    $scope.checkUser = function(){
+        console.log($scope.user);
+        $http({method: 'POST', url: '/log', data: $scope.user})
+            .then(function(data){
+                /// $scope.user.id = data;  ????? whay???
+                console.log("id exists!");
+                $scope.modalText = {
+                    headline: "LogIn",
+                    text: "התחברת בהצלחה"
                 };
-            });
-        }
+                window.alert("התחברת בהצלחה");
+                $('#myModal').modal('hide');
+                // $('#modal').modal('show')
+                // <a href="#" data-toggle="modal" data-target="#login-modal">התחבר</a>
+            })
+            .catch(function() {
+                $scope.modalText = {
+                    headline: "LogIn",
+                    text: "פרטים לא נכונים"
+                };
+                $('#modal').modal('show')
+                console.log("Unsuccessful login") });
     };
 
-
-});
+}]);
 
